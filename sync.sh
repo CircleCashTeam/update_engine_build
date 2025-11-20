9957cc02f738f41587d9f505016eaf43a04ff876
affggh <879632264@qq.com>
2025-11-20 20:27:09 +0800

    scripts: make script easy to use

--- sync.sh
+++ sync.sh
@@ -1,35 +1,6 @@
 #!/bin/bash
 
-function log() {
-    t=$1
-    shift 1
-    info=$@
-
-    color="\033[96m"
-
-    if [ "$t" == "INFO" ]; then
-        color="\033[94m"
-    elif [ "$t" == "WARNING" ]; then
-        color="\033[93m"
-    elif [ "$t" == "ERROR" ]; then
-        color="\033[91m"
-    elif [ "$t" == "SUCCESS" ]; then
-        color="\033[92m"
-    else
-        color="\033[95m"
-        t="DEBUG"
-    fi
-
-    now=$(date "+%Y-%m-%d %H:%M:%S")
-
-    echo -e "\033[96m${now}\033[0m [${color}${t}\033[0m] \033[36m${info}\033[0m"
-}
-
-logi() { log INFO $@; }
-loge() { log ERROR $@; }
-logw() { log WARNING $@; }
-logs() { log SUCCESS $@; }
-logd() { log DEBUG $@; }
+source "scripts/log.sh"
 
 TAG="android-16.0.0_r3"
 FETCH_FROM="https://mirrors.tuna.tsinghua.edu.cn/git/AOSP/"
