d970afa0ce1828b829d3c6643f60ec3f59015a17
affggh <879632264@qq.com>
2025-11-27 13:29:12 +0800

    cmake: silly I

--- cmake/liblz4.cmake
+++ cmake/liblz4.cmake
@@ -1,6 +1,10 @@
 set(target lz4)
 set(target_dir "${CMAKE_SOURCE_DIR}/platform/external/lz4/lib")
 
+set(target_cflags
+        "-Wall" "-Werror"
+)
+
 set(target_srcs
         "${target_dir}/lz4.c"
         "${target_dir}/lz4hc.c"
